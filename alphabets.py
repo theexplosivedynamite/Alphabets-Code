b5f5220c5bef2ae67e5b21f0d5bbfd5e7d9ec023
theexplosivedynamite <77196021+theexplosivedynamite@users.noreply.github.com>
2021-10-30 16:31:08 +0200

    Update alphabets.py

--- alphabets.py
+++ alphabets.py
@@ -52,4 +52,7 @@ X = "X"
 Y = "Y"
 Z = "Z"
 
+spacesout = " "
+
 alphabets = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+alphabets_capitals = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P ,Q ,R, S, T, U, V, W, X, Y, Z]
